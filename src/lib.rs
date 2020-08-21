edde016aabf32142ec990662a5bfde29a2e2f0d6
Spenser Black <spenserblack01@gmail.com>
2020-08-21 12:55:22 -0400

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -25,52 +25,52 @@
 //! ..o
 //! ..f\n");
 //! ```
-use itertools::Itertools;
 use itertools::EitherOrBoth::{Both, Left, Right};
+use itertools::Itertools;
 use std::fmt::Display;
 
 /// Places `foreground` "on top of" `background`.
-pub fn overlap<B, F>(background: B, foreground: F) -> String where B: Display, F: Display {
+pub fn overlap<B, F>(background: B, foreground: F) -> String
+where
+    B: Display,
+    F: Display,
+{
     let background = background.to_string();
     let foreground = foreground.to_string();
     let background = background.lines();
     let foreground = foreground.lines();
 
-    background.zip_longest(foreground).map(|lines| {
-        let combined_line: String = match lines {
-            Both(b_line, f_line) => {
-                let b_chars = b_line.chars();
-                let f_chars = f_line.chars();
+    background
+        .zip_longest(foreground)
+        .map(|lines| {
+            let combined_line: String = match lines {
+                Both(b_line, f_line) => {
+                    let b_chars = b_line.chars();
+                    let f_chars = f_line.chars();
 
-                let combined_line: String = b_chars
-                    .zip_longest(f_chars)
-                    .map(|chars| {
-                        match chars {
+                    let combined_line: String = b_chars
+                        .zip_longest(f_chars)
+                        .map(|chars| match chars {
                             Both(b_char, ' ') => b_char,
                             Both(_, f_char) => f_char,
                             Left(b_char) => b_char,
                             Right(f_char) => f_char,
-                        }
-                    })
-                    .collect();
-                combined_line
-            }
-            Left(b_line) => {
-                b_line.to_string()
-            }
-            Right(f_line) => {
-                f_line.to_string()
-            }
-        };
-        format!("{}\n", combined_line)
-    }).collect()
-
+                        })
+                        .collect();
+                    combined_line
+                }
+                Left(b_line) => b_line.to_string(),
+                Right(f_line) => f_line.to_string(),
+            };
+            format!("{}\n", combined_line)
+        })
+        .collect()
 }
 
 #[cfg(test)]
 mod tests {
-    use pretty_assertions::assert_eq;
     use super::overlap;
+    use pretty_assertions::assert_eq;
 
     #[test]
     fn it_works() {
