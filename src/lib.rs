6979ed3179a5c31cc01c1f671b1337fdb6a3c957
Spenser Black <spenserblack01@gmail.com>
2020-08-21 10:14:08 -0400

    Add failing test

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,19 @@
+use std::fmt::Display;
+
+pub fn overlap<B, F>(background: B, foreground: F) -> String where B: Display, F: Display {
+    "".into()
+}
+
 #[cfg(test)]
 mod tests {
+    use super::overlap;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        const BACKGROUND: &str = include_str!("../assets/passing/background.ascii");
+        const FOREGROUND: &str = include_str!("../assets/passing/foreground.ascii");
+        const EXPECTED: &str = include_str!("../assets/expected.ascii");
+
+        assert_eq!(overlap(BACKGROUND, FOREGROUND), EXPECTED);
     }
 }
