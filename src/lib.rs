289e5712611ef1fa501566c7cef205871e90406c
Spenser Black <spenserblack01@gmail.com>
2020-08-21 11:02:06 -0400

    Add overlapping logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,42 @@
+use itertools::Itertools;
+use itertools::EitherOrBoth::{Both, Left, Right};
 use std::fmt::Display;
 
+// NOTE Make &B and &F?
 pub fn overlap<B, F>(background: B, foreground: F) -> String where B: Display, F: Display {
-    "".into()
+    let background = background.to_string();
+    let foreground = foreground.to_string();
+    let background = background.lines();
+    let foreground = foreground.lines();
+
+    background.zip_longest(foreground).map(|lines| {
+        match lines {
+            Both(b_line, f_line) => {
+                let b_chars = b_line.chars();
+                let f_chars = f_line.chars();
+
+                let combined_line: String = b_chars
+                    .zip_longest(f_chars)
+                    .map(|chars| {
+                        match chars {
+                            Both(' ', f_char) => f_char,
+                            Both(b_char, _) => b_char,
+                            Left(b_char) => b_char,
+                            Right(f_char) => f_char,
+                        }
+                    })
+                    .collect();
+                combined_line
+            }
+            Left(b_line) => {
+                b_line.to_string()
+            }
+            Right(f_line) => {
+                f_line.to_string()
+            }
+        }
+    }).collect()
+
 }
 
 #[cfg(test)]
